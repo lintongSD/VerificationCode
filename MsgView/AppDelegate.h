3f9f4ca38ea9ef0884409ded5ef22f2594db6c8b
EBIZM2 <460561478@qq.com>
2019-02-20 11:09:15 +0800

    提交

---
+++ MsgView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MsgView
+//
+//  Created by EBIZM2 on 2019/2/19.
+//  Copyright © 2019年 EBIZM2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
